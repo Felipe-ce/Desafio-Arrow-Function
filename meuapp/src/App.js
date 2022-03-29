c64bb636736dba5137784e19b3db0ceae5c733de
Felipe Xavier <felipe_rsr@live.com>
2022-03-29 20:07:52 -0300

    update

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div></div>;
+//Crie um component de class
+class App extends Component {
+  // Crie uma arrow function que receba uma mensagem e exiba na tela
+  mensagem = () => {
+    const msg = 'Ola mundo';
+    return msg;
+  };
+
+  // Crie uma arrow function que retorne o dobro de um número e exiba na tela
+  dobroNumero = (numero) => {
+    return numero * 2;
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.mensagem()}</h1>
+        <h1>{this.dobroNumero(156)}</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
